ea47be1153ad566fbf56b80dcf2af86a7574aa04
Dimasacat-Andronicus <dmsctndrncs@gmail.com>
2025-03-19 11:30:12 +0800

    seperate the functions to the class

--- app/src/main/java/com/example/calculatorapp/OperationFunction.kt
+++ app/src/main/java/com/example/calculatorapp/OperationFunction.kt
@@ -1,2 +1,29 @@
 package com.example.calculatorapp
 
+fun add(num1: String, num2: String): Double {
+    val a = num1.toDouble()
+    val b = num2.toDouble()
+    return a + b
+}
+
+fun subtract(num1: String, num2: String): Double {
+    val a = num1.toDouble()
+    val b = num2.toDouble()
+    return a - b
+}
+
+fun multiply(num1: String, num2: String): Double {
+    val a = num1.toDouble()
+    val b = num2.toDouble()
+    return a * b
+}
+
+fun divide(num1: String, num2: String): Double {
+    val a = num1.toDouble()
+    val b = num2.toDouble()
+    return if (b == 0.0) {
+        Double.NaN
+    } else {
+        a / b
+    }
+}
